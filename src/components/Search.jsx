69e178ff21c41688db390ce56d8f2ca1cd6d867a
abdelmanaf <ourotoumanaf6@gmail.com>
2022-02-04 20:49:51 -0500

    add the fetch data method

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,24 @@
+import Word from './Words';
+import { useState } from 'react';
 import logo from '../logo.png'
 
 function Search() {
+    const [result, setResult] = useState({});
+
+
+    async function fetchData(val){
+        const trimVal = val.trim().toLowerCase();
+        if (trimVal.length > 0){
+            const api = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${trimVal}`;
+            const response = await fetch(api);
+            if (!response.ok){
+                throw Error (response.statusText)
+            }
+            return await response.json();
+        }
+    }
+    
+
     return (
         <div className="container">
             <div className="header">
